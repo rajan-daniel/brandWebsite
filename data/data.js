17a26cbab66410024070e487306faf719d6e4fcc
Rajan Daniel <rajan.daniel.dev@gmail.com>
2026-05-11 05:42:46 -0500

    chore: clean data.js file

--- data/data.js
+++ data/data.js
@@ -1,9 +1,8 @@
 export const about = {
   name: "brandStarter",
-  description: "Full Stack Web Developer",
-  about:
-    "hello! I’m Rajan, a full-stack web developer specializing in building modern, production-ready applications with React, Next.js, TypeScript, Tailwind, and Supabase. I develop end-to-end solutions—handling frontend, backend logic, APIs, authentication, and databases—while focusing on performance, scalability, and clean architecture. I’m driven by solving real problems and consistently improving through hands-on project development.",
-  email: "me@rajandaniel.com",
+  description: "",
+  about: "",
+  email: "",
 };
 
 export const seo = {
@@ -15,110 +14,5 @@ export const seo = {
 };
 
 export const forms = {
-  formspreeUrl: "https://formspree.io/f/xykokwby",
+  formspreeUrl: "",
 };
-
-export const skills = [
-  { name: "HTML", alt: "html logo", icon: "/html.svg" },
-  { name: "CSS", alt: "css logo", icon: "/css.svg" },
-  { name: "JS", alt: "js logo", icon: "/js.svg" },
-  { name: "React JS", alt: "react logo", icon: "/react.svg" },
-  { name: "TypeScript", alt: "typscript logo", icon: "/typscript.svg" },
-  { name: "Next JS", alt: "nextjs logo", icon: "/nextjs.svg" },
-  { name: "Tailwind", alt: "tailwind logo", icon: "/tailwind.svg" },
-  { name: "SupaBase", alt: "supabase logo", icon: "/supabase.svg" },
-];
-
-export const tools = [
-  { name: "Git", alt: "html logo", icon: "/git.svg" },
-  { name: "VS Code", alt: "css logo", icon: "/vscode.svg" },
-  { name: "Git Hub", alt: "js logo", icon: "/github.svg" },
-  { name: "Teams", alt: "react logo", icon: "/teams.svg" },
-  { name: "Zendesk", alt: "typscript logo", icon: "/zendesk.svg" },
-  { name: "Git Bash", alt: "nextjs logo", icon: "/gitbash.svg" },
-];
-
-export const socials = {
-  github: "https://github.com/rajan-daniel",
-  twitter: "",
-  linkedin: "https://www.linkedin.com/in/rajandaniel/",
-  facebook: "",
-};
-
-export const projects = {
-  pinProjects: [
-    {
-      title: "Personal Portfolio",
-      description:
-        "A sleek, modern portfolio site to showcasing my work and skills. Responsive design with smooth navigation and SEO optimization.",
-      tags: ["Next JS", "Tailwind CSS"],
-      link: "",
-    },
-  ],
-  otherProjects: [],
-};
-
-export const roadmap = [
-  {
-    title: "Introduction to Software Development",
-    description:
-      "First introduced to software development by scaffolding fundamental programming concepts using C++, Java & Discord.js. First small project was a discord bot made using Discord.js a Node.js library for developing Discord bots.",
-    date: "12/27/2015",
-  },
-  {
-    title: "Bachelor of Science in Computer Information Systems",
-    description:
-      "Focused on full-stack development using C++ and Java (NetBeans), with hands-on experience in MySQL databases. Coursework included object-oriented programming, backend development, database design, web security, cloud computing, and networking fundamentals.",
-    date: "02/24/2024",
-  },
-  {
-    title: "freeCodeCamp Certifications",
-    description: (
-      <>
-        <ul className="space-y-1">
-          <li>
-            <strong>Responsive Web Design:</strong> HTML5, CSS3, Flexbox, Grid,
-            accessibility, responsive layouts
-          </li>
-          <li>
-            <strong>JavaScript Algorithms and Data Structures:</strong> ES6+,
-            OOP, functional programming, data structures, algorithmic problem
-            solving
-          </li>
-          <li>
-            <strong>Front-End Development Libraries:</strong> React, Redux,
-            Bootstrap, jQuery, SASS, component-based UI development
-          </li>
-          <li>
-            <strong>Front-End:</strong> Capstone Certification
-          </li>
-        </ul>
-      </>
-    ),
-    date: "12/14/2025 - 04/18/2026",
-  },
-  {
-    title: "Project",
-    description:
-      "In progress...",
-    date: "🔴 IN PROGRESS 5/2/2026 - Present",
-  },
-];
-
-export const blogs = [
-  {
-    title: "Understand React Compnents",
-    date: "12/03/2025",
-    link: "#",
-  },
-  {
-    title: "CSS Grid vs Flexbox: When to Use Which",
-    date: "25/12/2024",
-    link: "#",
-  },
-  {
-    title: "Next.js SEO Best Practices",
-    date: "10/07/2024",
-    link: "#",
-  },
-];
