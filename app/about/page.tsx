0b383d331db0c89f40233555894053a42542426e
Rajan Daniel <rajan.daniel.dev@gmail.com>
2026-05-11 18:17:34 -0500

    feat: showcase about features and metrics

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,34 @@
-import { about } from "@/data/data";
+import { about, features, metrics } from "@/data/data";
 
 export default function About() {
-    return(
-        <>
-            <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-                <img src="/banner.webp"></img>
-                <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">{about.name}</h1>
-                <p>{about.description}</p>
+  return (
+    <>
+      <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <img src="/banner.webp"></img>
+        <h1 className="max-w-xs text-5xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          {about.name}
+        </h1>
+        <p>{about.description}</p>
+
+        <div className="flex gap-60">
+          {features.map((feature, index) => (
+            <div key={index} className="flex items-center justify-center flex-col">
+                <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">{feature.title}</h1>
+                <p>{feature.description}</p>
             </div>
-        </>
-    )
+          ))}
+        </div>
+
+        <div className="flex gap-60">
+          {metrics.map((metric, index) => (
+            <div key={index} className="flex items-center justify-center flex-col">
+                <h1 className="max-w-xs text-8xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">{metric.count}</h1>
+                <p className="text-3xl">{metric.description}</p>
+            </div>
+            
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
--- data/data.tsx
+++ data/data.tsx
@@ -1,7 +1,9 @@
+import { count } from "console";
+
 export const about = {
-  name: "brandStarter",
+  name: "siteSprout",
   role: "website template",
-  description: "brandStarter is known for delivering premium website scaffolds that make building a professional brand presence simple and fast.",
+  description: "siteSprout is known for delivering premium website scaffolds that make building a professional brand presence simple and fast.",
   about: "",
   email: "rajan.daniel.dev@gmailcom",
 };
@@ -9,10 +11,36 @@ export const about = {
 export const seo = {
   title: `${about.name} | ${about.role}`,
   description:
-    "Minimal, clean Next.js portfolio template for developers seeking a simple, professional online presence without extra complexity.",
+    "Minimal, clean Next.js brand website template for owners seeking a simple, professional online presence without extra complexity.",
   keywords:
-    "portfolio, web developer, Next.js, developer portfolio, minimal portfolio",
+    "brand website, brand developer, Next.js, brand portfolio, minimal portfolio",
 };
+
+export const features = [
+  {
+    title: "Feature 1",
+    description: "Feature 1 Desc",
+  },
+  {
+    title: "Feature 2",
+    description: "Feature 2 Desc",
+  },
+  {
+    title: "Feature 3",
+    description: "Feature 3 Desc",
+  },
+];
+
+export const metrics = [
+  {
+    count: "123",
+    description: "Downloads Daily",
+  },
+  {
+    count: "123456",
+    description: "Downloads Monthly",
+  },
+]
 
 export const forms = {
   formspreeUrl: "",
