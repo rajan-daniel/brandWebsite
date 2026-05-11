2049cce1e3cd64be2a013039d4246dfca1d481e2
Rajan Daniel <rajan.daniel.dev@gmail.com>
2026-05-11 06:04:31 -0500

    feat: about page container

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,11 @@
 export default function About() {
     return(
         <>
-            <h1>About Page</h1>
+            <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+                <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Welcome!
+          </h1>
+            </div>
         </>
     )
 }
--- data/data.tsx
+++ data/data.tsx
@@ -1,5 +1,6 @@
 export const about = {
   name: "brandStarter",
+  role: "website template",
   description: "",
   about: "",
   email: "",
