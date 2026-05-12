c4eea7c34e02de251d68e6213bf4dc86bfde36f5
Rajan Daniel <rajan.daniel.dev@gmail.com>
2026-05-12 14:07:07 -0500

    test: products grid

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,17 @@
+import { products } from "@/data/data";
+
 export default function Products() {
-    return(
-        <>
-            <h1>Products Page</h1>
-        </>
-    )
+  return (
+    <>
+      <div className="grid grid-cols-4 gap-8 justify-center place-items-center bg-zinc-50 font-sans dark:bg-white px-6 pt-24 pb-16">
+        {products.map((product, index) => (
+          <div key={index}>
+            <img src={product.image} className="w-64 h-64 object-cover transition-transform duration-300 hover:scale-110 " />
+            <h1 className="text-2xl font-semibold text-[var(--text-color)] pt-8">{product.title}</h1>
+            <p className="text-[var(--text-color)]">{product.price}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
--- data/data.tsx
+++ data/data.tsx
@@ -1,7 +1,8 @@
 export const about = {
   name: "siteSprout",
   role: "website template",
-  description: "siteSprout is known for delivering a premium website scaffold that make building a professional brand presence simple and fast.",
+  description:
+    "siteSprout is known for delivering a premium website scaffold that make building a professional brand presence simple and fast.",
   about: "",
   email: "rajan.daniel.dev@gmail.com",
 };
@@ -17,15 +18,18 @@ export const seo = {
 export const features = [
   {
     title: "Housing top scaffolds",
-    description: "SiteSprout is a personal project built to bring together the essential foundation for modern brand websites. It’s designed as a clean, structured starting point for creators and developers who want to launch fast, with clarity, flexibility, and scalable design built for real-world projects.",
+    description:
+      "SiteSprout is a personal project built to bring together the essential foundation for modern brand websites. It’s designed as a clean, structured starting point for creators and developers who want to launch fast, with clarity, flexibility, and scalable design built for real-world projects.",
   },
   {
     title: "Crafting premium websites",
-    description: "I focus on building high-quality website foundations that meet the needs of modern brands and developers. SiteSprout is designed to enhance performance where it counts, delivering a clean, scalable scaffold that helps you build faster and with confidence.",
+    description:
+      "I focus on building high-quality website foundations that meet the needs of modern brands and developers. SiteSprout is designed to enhance performance where it counts, delivering a clean, scalable scaffold that helps you build faster and with confidence.",
   },
   {
     title: "Advancing web creation",
-    description: "Through thoughtful structure and modern tooling, I aim to push the boundaries of how brand websites are built with SiteSprout, helping improve the ecosystem one project at a time through a solo-built foundation.",
+    description:
+      "Through thoughtful structure and modern tooling, I aim to push the boundaries of how brand websites are built with SiteSprout, helping improve the ecosystem one project at a time through a solo-built foundation.",
   },
 ];
 
@@ -38,34 +42,64 @@ export const metrics = [
     count: "12M+",
     description: "Downloads Monthly",
   },
-]
+];
 
 export const products = [
   {
-
+    title: "service 1",
+    price: "25",
+    image: "/products/product1.webp",
+    description: "This is a test description of the product 1",
   },
   {
-
+    title: "service 2",
+    price: "125",
+    image: "/products/product1.webp",
+    description: "This is a test description of the product 1",
   },
   {
-
+    title: "service 2",
+    price: "125",
+    image: "/products/product1.webp",
+    description: "This is a test description of the product 1",
   },
   {
-
+    title: "service 2",
+    price: "125",
+    image: "/products/product1.webp",
+    description: "This is a test description of the product 1",
   },
   {
-
+    title: "service 2",
+    price: "125",
+    image: "/products/product1.webp",
+    description: "This is a test description of the product 1",
   },
   {
-
+    title: "service 2",
+    price: "125",
+    image: "/products/product1.webp",
+    description: "This is a test description of the product 1",
   },
   {
-
+    title: "service 2",
+    price: "125",
+    image: "/products/product1.webp",
+    description: "This is a test description of the product 1",
   },
   {
-
+    title: "service 2",
+    price: "125",
+    image: "/products/product1.webp",
+    description: "This is a test description of the product 1",
   },
-]
+  {
+    title: "service 2",
+    price: "125",
+    image: "/products/product1.webp",
+    description: "This is a test description of the product 1",
+  },
+];
 export const forms = {
   formspreeUrl: "",
 };
