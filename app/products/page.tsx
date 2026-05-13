37812b6ddd8883d9305d10e02a002f15fc71c2e5
Rajan Daniel <rajan.daniel.dev@gmail.com>
2026-05-12 19:01:16 -0500

    style: products page background

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,10 +3,10 @@ import { products } from "@/data/data";
 export default function Products() {
   return (
     <>
-      <div className="grid grid-cols-4 gap-8 justify-center place-items-center bg-zinc-50 font-sans dark:bg-white px-6 pt-24 pb-16">
+      <div className="bg-white dark:bg-white grid grid-cols-4 gap-8 place-items-center max-w-7xl mx-auto font-sans px-6 pt-28 pb-16">
         {products.map((product, index) => (
-          <div key={index}>
-            <img src={product.image} className="w-64 h-64 object-cover transition-transform duration-300 hover:scale-110 " />
+          <div key={index} className="w-full max-w-[272px]">
+            <img src={product.image} className="w-68 h-68 object-cover transition-transform duration-300 hover:scale-110 " />
             <h1 className="text-2xl font-semibold text-[var(--text-color)] pt-8">{product.title}</h1>
             <p className="text-[var(--text-color)]">{product.price}</p>
           </div>
