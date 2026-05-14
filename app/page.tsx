cd425c4bca42ce309bc66c0af1955ce8e5512d67
codingBatWarrior <rajan.daniel.dev@gmail.com>
2026-05-14 13:02:00 -0500

    test: homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center bg-zinc-50 font-sans dark:bg-white px-6 py-16 gap-12">
-      <div className="w-full max-w-6xl rounded-3xl shadow-[0_25px_80px_rgba(0,0,0,0.25)]">
-         
+    <main className="relative h-screen w-screen overflow-hidden">
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute inset-0 h-full w-full object-cover grayscale"
+      >
+        <source src="/home.mp4" type="video/mp4" />
+      </video>
+
+      <div className="absolute inset-0 bg-black/40" />
+
+      <div className="relative z-10 flex h-full items-center justify-center">
+        <h1 className="text-7xl font-bold text-white">
+          Welcome to siteSprout
+        </h1>
       </div>
-    </div>
+    </main>
   );
 }
