2ee79ab953b92beb4c972fdc3ce435ee525f08e6
Rajan Daniel <rajan.daniel.dev@gmail.com>
2026-05-17 10:32:10 -0500

    fix: flex layout for individual products

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -8,59 +8,61 @@ export default async function ProductPage({
   const { id } = await params;
   const product = products.find((product) => product.id === id);
   return (
-    <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-[1.35fr_1fr] gap-24 px-8 py-12">
-      <div className="lg:sticky lg:top-24 self-start">
-        <img
-          src={product?.image}
-          className="
+    <main className="flex-1">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-[1.35fr_1fr] gap-24 px-8 py-12">
+        <div className="lg:sticky lg:top-24 self-start">
+          <img
+            src={product?.image}
+            className="
     w-full
     max-h-[650px]
     object-contain
     rounded-2xl
     bg-white
   "
-        />
-      </div>
+          />
+        </div>
 
-      <div className="flex flex-col max-w-xl gap-4">
-        <h1
-          className="
+        <div className="flex flex-col max-w-xl gap-4">
+          <h1
+            className="
             text-4xl
             lg:text-3xl
             font-semibold
             leading-tight
             text-[var(--text-color)]
           "
-        >
-          {product?.title}
-        </h1>
+          >
+            {product?.title}
+          </h1>
 
-        <p
-          className="
+          <p
+            className="
             text-lg
             text-[var(--text-color)]
             opacity-90
           "
-        >
-          ${product?.price} {currency}
-        </p>
+          >
+            ${product?.price} {currency}
+          </p>
 
-        <div
-          className="
+          <div
+            className="
             pt-32
             text-sm
             leading-6
             text-[var(--text-color)]
           "
-        >
-          <div
-            className="space-y-6"
-            dangerouslySetInnerHTML={{
-              __html: product?.description ?? "",
-            }}
-          />
+          >
+            <div
+              className="space-y-6"
+              dangerouslySetInnerHTML={{
+                __html: product?.description ?? "",
+              }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,38 +4,40 @@ import { products, currency, booking } from "@/data/data";
 export default function Products() {
   return (
     <>
-      <div className="bg-white dark:bg-white max-w-7xl mx-auto px-6 pt-28 pb-16">
-        <div className="grid gap-8 grid-cols-[repeat(4,1fr)] max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1">
-          {products.map((product, index) => (
-            <div key={index} className="w-full">
-              <Link href={`/products/${product.id}`}>
-                <img
-                  src={product.image}
-                  className="w-full aspect-square object-cover rounded-xl transition-transform duration-300 ease-in-out hover:scale-105"
-                />
-              </Link>
+      <main className="flex-1">
+        <div className="bg-white dark:bg-white max-w-7xl mx-auto px-6 pt-28 pb-16">
+          <div className="grid gap-8 grid-cols-[repeat(4,1fr)] max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1">
+            {products.map((product, index) => (
+              <div key={index} className="w-full">
+                <Link href={`/products/${product.id}`}>
+                  <img
+                    src={product.image}
+                    className="w-full aspect-square object-cover rounded-xl transition-transform duration-300 ease-in-out hover:scale-105"
+                  />
+                </Link>
 
-              <h1 className="text-l font-bold text-[var(--text-color)] pt-5">
-                <Link href={`/products/${product.id}`}>{product.title}</Link>
-              </h1>
+                <h1 className="text-l font-bold text-[var(--text-color)] pt-5">
+                  <Link href={`/products/${product.id}`}>{product.title}</Link>
+                </h1>
 
-              <p className="text-[var(--text-color)]">
-                ${product.price} {currency}
-              </p>
-            </div>
-          ))}
+                <p className="text-[var(--text-color)]">
+                  ${product.price} {currency}
+                </p>
+              </div>
+            ))}
+          </div>
+          {booking.show && (
+            <Link
+              href={booking.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-10 mx-auto block w-fit px-8 py-4 text-lg font-bold text-[var(--text-color)] bg-[var(--accent-silver)] rounded-full hover:scale-105 transition-transform duration-300"
+            >
+              {booking.buttonName}
+            </Link>
+          )}
         </div>
-        {booking.show && (
-          <Link
-            href={booking.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mt-10 mx-auto block w-fit px-8 py-4 text-lg font-bold text-[var(--text-color)] bg-[var(--accent-silver)] rounded-full hover:scale-105 transition-transform duration-300"
-          >
-            {booking.buttonName}
-          </Link>
-        )}
-      </div>
+      </main>
     </>
   );
 }
